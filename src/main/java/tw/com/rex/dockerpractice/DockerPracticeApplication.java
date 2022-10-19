a7eb2ee2f0626b1f7e7dd8c1444e52b51bd288c8
rex <yu.chenhang@gmail.com>
2022-10-19 22:14:57 +0800

    init

---
+++ src/main/java/tw/com/rex/dockerpractice/DockerPracticeApplication.java
@@ -0,0 +1,13 @@
+package tw.com.rex.dockerpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerPracticeApplication.class, args);
+    }
+
+}
