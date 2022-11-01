a046bbe465fbcf9c637c1bb39567407f534549e0
rex <yu.chenhang@gmail.com>
2022-11-01 15:42:04 +0800

    add get active profiles

--- src/main/java/tw/com/rex/dockerpractice/web/controller/HelloController.java
+++ src/main/java/tw/com/rex/dockerpractice/web/controller/HelloController.java
@@ -1,17 +1,27 @@
 package tw.com.rex.dockerpractice.web.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/test")
 public class HelloController {
 
+    private Environment environment;
+
     @GetMapping("/hi")
     public ResponseEntity<String> sayHi() {
         return ResponseEntity.ok("Hi~");
+    }
+
+    @GetMapping("/profiles")
+    public ResponseEntity<String[]> getCurrentProfile() {
+        return ResponseEntity.ok(environment.getActiveProfiles());
     }
 
 }
